75079679c2a1986a5d6c28952165603a3b956467
Sahil <sahilnanda1995@gmail.com>
2021-07-06 15:53:39 +0530

    feat: 🎸 added seo

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+export default {
+	defaultTitle: "YieldScan",
+	openGraph: {
+		type: "website",
+		locale: "en_IE",
+		url: "https://yieldscan.app/",
+		site_name: "YieldScan",
+		description:
+			"Polkadot Staking - YieldScan: Built to maximize staking yield, Designed to minimize effort",
+	},
+	twitter: {
+		handle: "@YieldScan",
+		site: "@YieldScan",
+		cardType: "app",
+	},
+};
