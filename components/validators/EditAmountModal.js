c29bd5f37ddcf0665fa295bbf98ec4c582634ea3
Lord-of-Codes <suryansh.80@gmail.com>
2021-08-27 22:14:49 +0530

    added lowBalanceAlert component to editAmountModal component

--- components/validators/EditAmountModal.js
+++ components/validators/EditAmountModal.js
@@ -20,6 +20,7 @@ import withSlideIn from "@components/common/withSlideIn";
 import { isNil, get } from "lodash";
 import { useCoinGeckoPriceUSD } from "@lib/store";
 import formatCurrency from "@lib/format-currency";
+import LowBalanceAlert from "@components/reward-calculator/LowBalanceAlert";
 
 const EditAmountModal = withSlideIn(
 	({
@@ -35,6 +36,8 @@ const EditAmountModal = withSlideIn(
 		networkInfo,
 		trackRewardCalculatedEvent,
 		minPossibleStake,
+		controllerUnavailable,
+		controllerBalances,
 	}) => {
 		const { coinGeckoPriceUSD } = useCoinGeckoPriceUSD();
 		const [stakingAmount, setStakingAmount] = useState(() => amount);
@@ -93,78 +96,23 @@ const EditAmountModal = withSlideIn(
 					<ModalCloseButton onClick={onClose} />
 					<ModalBody>
 						<div className="mt-4">
-							{selectedAccount &&
-							stakingAmount >
-								totalPossibleStakingAmount - networkInfo.reserveAmount ? (
-								<Alert
-									status="error"
-									rounded="md"
-									flex
-									flexDirection="column"
-									alignItems="start"
-									my={4}
-								>
-									<AlertTitle color="red.500">
-										<span className="text-base">Insufficient Balance</span>
-									</AlertTitle>
-									<AlertDescription color="red.500">
-										<span className="text-sm">
-											We cannot stake this amount since we recommend maintaining
-											a minimum balance of {networkInfo.reserveAmount}{" "}
-											{networkInfo.denom} in your account at all times.{" "}
-										</span>
-										<Popover trigger="hover" usePortal>
-											<PopoverTrigger>
-												<span className="underline cursor-help text-sm">
-													Why?
-												</span>
-											</PopoverTrigger>
-											<PopoverContent
-												zIndex={99999}
-												_focus={{ outline: "none" }}
-												bg="gray.700"
-												border="none"
-											>
-												<PopoverArrow />
-												<PopoverBody>
-													<span className="text-white text-xs">
-														This is to ensure that you have a decent amout of
-														funds in your account to pay transaction fees for
-														claiming rewards, unbonding funds, changing on-chain
-														staking preferences, etc.
-													</span>
-												</PopoverBody>
-											</PopoverContent>
-										</Popover>
-									</AlertDescription>
-								</Alert>
-							) : (
-								selectedAccount &&
-								stakingAmount < minPossibleStake && (
-									<Alert
-										status="error"
-										rounded="md"
-										flex
-										flexDirection="column"
-										alignItems="start"
-										my={4}
-									>
-										<AlertTitle color="red.500">
-											<span className="text-base">
-												{activeBondedAmount > 0
-													? "Current amount insufficient to stake anymore"
-													: "Amount insufficient to begin staking"}
-											</span>
-										</AlertTitle>
-										<AlertDescription color="red.500">
-											<span className="text-sm">
-												We cannot stake this amount as it does not cross the{" "}
-												{minPossibleStake} {networkInfo.denom} minimum staking
-												threshold mandated by the {networkInfo.name} network.{" "}
-											</span>
-										</AlertDescription>
-									</Alert>
-								)
+							{selectedAccount && balances && stakingInfo && (
+								<LowBalanceAlert
+									amount={amount}
+									activeBondedAmount={activeBondedAmount}
+									networkInfo={networkInfo}
+									totalPossibleStakingAmount={totalPossibleStakingAmount}
+									totalAvailableStakingAmount={totalAvailableStakingAmount}
+									minPossibleStake={minPossibleStake}
+									controllerUnavailable={controllerUnavailable}
+									controllerAvailableAmount={
+										controllerBalances
+											? (parseInt(controllerBalances?.availableBalance) -
+													apiInstance?.consts.balances.existentialDeposit.toNumber()) /
+											  Math.pow(10, networkInfo.decimalPlaces)
+											: null
+									}
+								/>
 							)}
 							<div
 								className="m-2 text-gray-600 text-sm"
